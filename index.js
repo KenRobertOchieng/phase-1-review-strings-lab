b4962ef01ada36f4f646365d650919795b4f4096
Ken Robert Ochieng <kenrobertochi@gmail.com>
2025-03-04 11:59:03 +0300

    learnt strings

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let currentUser='Ken Robert'
+let welcomeMessage="Welcome to Flatbook, " + currentUser+"!"
+let excitedWelcomeMessage="WELCOME TO FLATBOOK, " +"KEN ROBERT"+"!"
+let shortGreeting=`Welcome, ${currentUser.slice(0,1)}!`
